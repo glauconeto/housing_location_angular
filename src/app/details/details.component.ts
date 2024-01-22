e7cbd17f1679fb0154d5c0d4be6f07991b49967a
glauconeto <glauco.neto@outlook.com>
2024-01-22 08:51:37 -0300

    Adicionando formulário na página de detalhes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,23 +3,35 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
 
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingService = inject(HousingService);
-  housingLocation: HousingLocation | undefined;
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute)
+  housingService = inject(HousingService)
+  housingLocation: HousingLocation | undefined
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10)
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
   }
-
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
+  }
 }
