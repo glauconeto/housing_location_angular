99c8f5ea5c047110897ef58e106d23efbfdeb551
glauconeto <glauco.neto@outlook.com>
2024-01-22 09:52:52 -0300

    Atualizei o nome da aplicação

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'first_app' title`, () => {
+  it(`should have the 'housing_location' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first_app');
+    expect(app.title).toEqual('housing_location');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first_app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, housing_location');
   });
 });
